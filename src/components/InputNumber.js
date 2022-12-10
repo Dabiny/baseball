d53baf91b78cdf74caed5dc30d088bc055e6e7f2
Dabiny <lina917@naver.com>
2022-12-10 21:05:43 +0900

    -

--- src/components/InputNumber.js
+++ src/components/InputNumber.js
@@ -2,9 +2,9 @@ import '../assets/style/input.scss';
 const InputNumber = ({
   inputvalue, // 인풋창 밸류값 변경 state
   handleOnChange, // 인풋창이 바뀔때마다 setState해주는 함수.
-  isSuccess,
-  handleOnClick,
-  handleInitialize
+  isSuccess, // 버튼의 조건렌더링을 위해 필요한 state
+  handleOnClick, // swing버튼을 눌렀을때 바로 조건검사를 실행해주는 이벤트함수
+  handleInitialize // 다시하기 버튼을 눌렀을때 초기화해주는 이벤트함수
 }) => {
   return (
     <div className='input-section'>
